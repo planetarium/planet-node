8d15d70ec7ad7cc5bb4053abcf6f915a879d11a2
Ohjin <qpqpq13579@gmail.com>
2022-07-31 21:31:31 +0900

    Add Console.WriteLine(peerString)
    when _swarm is started

--- Libplanet.Headless/Hosting/SwarmService.cs
+++ Libplanet.Headless/Hosting/SwarmService.cs
@@ -14,8 +14,23 @@ public class SwarmService<T> : BackgroundService, IDisposable
         _swarm = swarm;
     }
 
+    private string getPeerString(Peer peer)
+    {
+        var pubKey = peer.PublicKey.ToString();
+        var hostAndPort = peer.ToString().Split('/')[1];
+        var host = hostAndPort.Split(':')[0];
+        var port = hostAndPort.Split(':')[1];
+        return $"peerString: {pubKey},{host},{port}";
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _ = _swarm.WaitForRunningAsync().ContinueWith(_ =>
+        {
+            var peer = _swarm.AsPeer;
+            var result = getPeerString(peer);
+            Console.WriteLine(result);
+        });
         await _swarm.PreloadAsync(cancellationToken: stoppingToken).ConfigureAwait(false);
         await _swarm.StartAsync(cancellationToken: stoppingToken).ConfigureAwait(false);
     }
