83cb416bae658199c24b4fd2aaf3ad23dde3fb39
Kwon Ohjin <61285931+zeta6@users.noreply.github.com>
2022-09-04 14:13:38 +0900

    Add peerString check (#9) (#13)
    * Add peerString
    
    * Add Console.WriteLine(peerString)
    
    when _swarm is started
    
    Co-authored-by: Hong Minhee (洪 民憙) <hong.minhee@planetariumhq.com>

--- Libplanet.Headless/Hosting/SwarmService.cs
+++ Libplanet.Headless/Hosting/SwarmService.cs
@@ -16,8 +16,23 @@ public class SwarmService<T> : BackgroundService, IDisposable
         _peers = peers;
     }
 
+    private string getPeerString(Peer peer)
+    {
+        var pubKey = peer.PublicKey.ToString();
+        var hostAndPort = peer.ToString().Split('/')[1];
+        var host = hostAndPort.Split(':')[0];
+        var port = hostAndPort.Split(':')[1];
+        return $"peerString: {pubKey},{host},{port}";
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _ = _swarm.WaitForRunningAsync().ContinueWith(_ =>
+        {
+            var peer = _swarm.AsPeer;
+            var result = getPeerString(peer);
+            Console.WriteLine(result);
+        });
         await _swarm.AddPeersAsync(_peers, default, cancellationToken: stoppingToken).ConfigureAwait(false);
         await _swarm.PreloadAsync(cancellationToken: stoppingToken).ConfigureAwait(false);
         await _swarm.StartAsync(cancellationToken: stoppingToken).ConfigureAwait(false);
